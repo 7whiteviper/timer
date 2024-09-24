00f95bbed12ff2561b5a5ceb0c30e15a4e99704f
7whiteviper <khatrib@clarkson.edu>
2024-09-24 08:24:07 -0400

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 
+void setTimer(int seconds) {
+  for (int i = seconds; i > 0; --i) {
+    std::cout << "Time remaining: " << i << " seconds\r";
+    std::cout.flush();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+  std::cout << "Time's up!      " << std::endl;
+}
+
+int main(){
+  int seconds;
+  std::cout << "Enter timer duration in seconds: ";
+  std::cin >> seconds;
+
+  setTimer(seconds);
+  return 0;
+}
